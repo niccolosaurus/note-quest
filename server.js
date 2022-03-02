2aada24cd4226746ecb59ed78ec13af85856ac5c
Niccolo Eck <nicco.eck@gmail.com>
2022-03-01 23:53:22 -0800

    second commit changing some code

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // Require Links
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
 
 //Linking the server
 //3001 is th one we used constantly in class, I don't belive it matters though
@@ -14,11 +12,11 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 //Routing for Files
-require("./route/api-route")(app);
-require("./route/html-route")(app);
+require("./api-route")(app);
+require("./html-route")(app);
 
 
 //Listener Function
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, () => {
+    console.log("App listening on http://localhost: " + PORT);
 });
